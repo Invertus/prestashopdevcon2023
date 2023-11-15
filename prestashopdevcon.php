e8351de32ffd49c1666336ccfb5aa503f27b7c0b
Tomas <tilginis@gmail.com>
2023-11-15 22:20:48 +0200

    adds title in one of the hooks

--- prestashopdevcon.php
+++ prestashopdevcon.php
@@ -11,11 +11,24 @@ class PrestashopDevCon extends Module
 
     public function install()
     {
-        return parent::install() && $this->registerHook('displayHome');
+        return parent::install()
+            && $this->registerHook('displayHome')
+            && $this->registerHook('displayCheckoutSummaryTop');
     }
 
     public function hookDisplayHome()
     {
         return "Hello from {$this->name}!";
+    }
+
+    public function hookDisplayCheckoutSummaryTop()
+    {
+        $text = "";
+        if ($this->context->controller instanceof OrderControllerCore) {
+            $checkourProcess  =$this->context->controller->getCheckoutProcess();
+            $text .= "We are currently at step: " . $checkourProcess->getCurrentStep()->getTitle();
+        }
+
+        return $text;
     }
 }
